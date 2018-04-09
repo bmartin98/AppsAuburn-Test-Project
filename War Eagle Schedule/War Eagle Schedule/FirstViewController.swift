2b8a2b96989f68d6ee5eb27cf85afc7e769a106c
Brandon Martin <btm0033@auburn.edu>
2018-04-09 07:59:06 -0500

    updated code

--- War Eagle Schedule/War Eagle Schedule/FirstViewController.swift
+++ War Eagle Schedule/War Eagle Schedule/FirstViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var mondayI: UIImageView!
+    @IBOutlet weak var mondayL: UILabel!
+    
+    let weekdays = ["Monday", "Tuesday", "Wendesday", "Thursday", "Friday"]
+    
+    let myImage: [UIImage] = [#imageLiteral(resourceName: "Mondau.gif"),#imageLiteral(resourceName: "Tuesday.jpg"),#imageLiteral(resourceName: "Wenesday.png"),#imageLiteral(resourceName: "Thursday.jpg"),#imageLiteral(resourceName: "Friday.jpg")]
+    
+    var myIndex = 0
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weekdays.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "monday")
+        
+        cell?.detailTextLabel?.text = weekdays[indexPath.row]
+        cell?.textLabel?.text =  weekdays[indexPath.row]
+        cell?.imageView?.image = myImage[indexPath.row]
+        return cell!
+    }
+     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        myIndex = indexPath.row
+        performSegue(withIdentifier: "Eagle", sender: self)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
--- War Eagle Schedule/War Eagle Schedule/SecondViewController.swift
+++ War Eagle Schedule/War Eagle Schedule/SecondViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let edit = ["Adding a class","Deleting a class","Editing a class"]
+    
+    var aIndex = 0
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return edit.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "edits")
+        
+        cell?.textLabel?.text =  edit[indexPath.row]
+        return cell!
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        aIndex = indexPath.row
+        performSegue(withIdentifier: "next", sender: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
